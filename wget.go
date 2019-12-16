74e8d54ad26c28212a8be291357f4e03a10e87dd
Nikita Atroshenko <nikitatroshenko@gmail.com>
2019-12-16 20:47:30 +0300

    Removed binary file Minimal working wget

--- wget.go
+++ wget.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
 func main() {
-	$END$
+	uri := os.Args[1]
+	resp, err := http.Get(uri)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = resp.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", body)
 }
