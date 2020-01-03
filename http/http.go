9bdeed5e0857f4bf118fd5ce6672fbfd4e5612b1
Nikita Atroshenko <nikitatroshenko@gmail.com>
2020-01-03 19:39:59 +0300

    Added support for specifying request body using --body-data

--- http/http.go
+++ http/http.go
@@ -4,14 +4,16 @@ import (
 	"flag"
 	"io"
 	"net/http"
+	"strings"
 )
 
 var method = flag.String("method", "GET", "HTTP method to send to server")
+var bodyData = flag.String("body-data", "", "String that needs to be send along with Method specified using --method")
 
 // WgetHttpResource performs HTTP request to the specified URL and saves response to the provided writer
 // HTTP method is passed through command line arguments
 func WgetHttpResource(rawurl string, writer io.Writer) (written int64, err error) {
-	req, err := http.NewRequest(*method, rawurl, nil)
+	req, err := http.NewRequest(*method, rawurl, strings.NewReader(*bodyData))
 	if err != nil {
 		return 0, err
 	}
